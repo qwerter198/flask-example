596c7fc1735fe9c56cb9169a36220cffcca89028
Louis <qwerter198@gmail.com>
2023-08-10 00:57:32 +0800

    fix: 調整ssh指令避免出現交互式提示

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "${SSH_KEY}" | base64 --decode > /temp/ssh_rsa
 chmod 600 /temp/ssh_rsa
 
 # 使用私钥连接到服务器并执行部署命令
-ssh -i /temp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
+ssh -i /temp/ssh_rsa -o StrictHostKeyChecking=no zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
 
 # 清除臨時文件
 rm /temp/ssh_rsa
