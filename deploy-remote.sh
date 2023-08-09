119099929c842b6dff42467f47ddb3b55c8455b8
Louis <qwerter198@gmail.com>
2023-08-10 01:52:28 +0800

    fix: 新增log來查看錯誤問題

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "cd /temp/flask-example"
 cd /temp/flask-example
 
+echo "git pull"
 git pull
 
+echo "docker-compose -f dokcer-compose.yml down"
 docker-compose -f dokcer-compose.yml down
 
+echo "docker rmi flask-example_web"
 docker rmi flask-example_web
 
+echo "docker-compose -f docker-compose.yml up -d"
 docker-compose -f docker-compose.yml up -d
 
 echo "Application deployed successfully!"
--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,16 @@
 
 echo "Deploying application to GCP server..."
 
+echo "${SSH_KEY}"
 echo "${SSH_KEY}" | base64 --decode > /tmp/ssh_rsa
+
 chmod 600 /tmp/ssh_rsa
 
 # 使用私鑰連接到server並執行佈署命令
+echo "ssh -i /tmp/ssh_rsa -o StrictHostKeyChecking=no zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
 ssh -i /tmp/ssh_rsa -o StrictHostKeyChecking=no zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
 
+
 # 清除臨時文件
+echo "rm /tmp/ssh_rsa"
 rm /tmp/ssh_rsa
