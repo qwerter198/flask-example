a3897ffd96ed7aa13c0332da6350a5b9e109cf47
Louis <qwerter198@gmail.com>
2023-08-12 03:20:31 +0800

    fix: 新增log尋找錯誤問題

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ echo "${SSH_KEY}" | base64 --decode > /tmp/ssh_rsa
 find /tmp/ssh_rsa
 chmod 600 /tmp/ssh_rsa
 
-# 使用私鑰連接到server並執行佈署命令
-echo "ssh -o StrictHostKeyChecking=no -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
+cat /tmp/ssh_rsa
 
-ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
+# 使用私鑰連接到server並執行佈署命令
+echo "ssh -o StrictHostKeyChecking no -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
+
+ssh -o "StrictHostKeyChecking no" -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
 
 # 清除臨時文件
 echo "rm /tmp/ssh_rsa"
