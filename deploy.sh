f6f76c09ca49034213a91e35afc9c896f38a29dd
Louis <qwerter198@gmail.com>
2023-08-12 02:54:57 +0800

    fix: 調整ssh指令

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,9 @@ echo "${SSH_KEY}" | base64 --decode > /tmp/ssh_rsa
 chmod 600 /tmp/ssh_rsa
 
 # 使用私鑰連接到server並執行佈署命令
-echo "ssh -i /tmp/ssh_rsa -o StrictHostKeyChecking=no zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
+echo "ssh -o StrictHostKeyChecking=no -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
 
-ssh -o "StrictHostKeyChecking no" zxvc198@${SERVER_IP} -i /tmp/ssh_rsa "echo test > test.log"
-ssh -o "StrictHostKeyChecking no" zxvc198@${SERVER_IP} -i /tmp/ssh_rsa 'bash -s' < deploy-remote.sh
+ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
 
 # 清除臨時文件
 echo "rm /tmp/ssh_rsa"
