c4be0ee8ba6adf540d47c744757f7a7e7cc53718
Louis <qwerter198@gmail.com>
2023-08-12 17:32:13 +0800

    refactor: 移除log資料

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,11 @@
 
 echo "Deploying application to GCP server..."
 
-echo "echo ${SSH_KEY} | base64 --decode > /tmp/ssh_rsa"
 echo "${SSH_KEY}" | base64 --decode > /tmp/ssh_rsa
 
-find /tmp/ssh_rsa
-chmod 600 /tmp/ssh_rsa
-
-cat /tmp/ssh_rsa
+chmod 600 ~/Desktop/id_rsa
 
 # 使用私鑰連接到server並執行佈署命令
-echo "echo ssh -o \"StrictHostKeyChecking=no\" -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh"
-
 ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa zxvc198@${SERVER_IP} 'bash -s' < deploy-remote.sh
-
 # 清除臨時文件
-echo "rm /tmp/ssh_rsa"
 rm /tmp/ssh_rsa
