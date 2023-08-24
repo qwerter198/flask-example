47bd42c0594183a737828dff43cfebb6e4ed00b3
Louis <qwerter198@gmail.com>
2023-08-24 23:33:43 +0800

    feat: 新增GCP kubernetes權限

--- deploy-remote-k8s.sh
+++ deploy-remote-k8s.sh
@@ -14,6 +14,9 @@ docker-compose push
 
 sleep 10
 
+gcloud auth activate-service-account --key-file=/tmp/gcp_key.json
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $CLUSTER_ZONE --project $PROJECT_ID
+
 kubectl apply -f kubernetes/deployment.yaml
 
 kubectl apply -f kubernetes/service.yaml
