2ad838d532b0765d2fa3aefd5b894b300ed8c016
Louis <qwerter198@gmail.com>
2023-08-25 00:36:34 +0800

    fix: 調整k8s流程並更新image測試

--- deploy-remote-k8s.sh
+++ deploy-remote-k8s.sh
@@ -24,15 +24,24 @@ kubectl version
 gcloud components list | grep gke-gcloud-auth-plugin
 
 gcloud auth activate-service-account --key-file=/tmp/gcp_key.json
-gcloud --quiet config set project ${PROJECT_ID}
-gcloud --quiet config set container/cluster ${CLUSTER_NAME}
-gcloud --quiet config set compute/zone ${CLUSTER_ZONE}
-gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
+# gcloud --quiet config set project ${PROJECT_ID}
+# gcloud --quiet config set container/cluster ${CLUSTER_NAME}
+# gcloud --quiet config set compute/zone ${CLUSTER_ZONE}
+# gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
+
+gcloud --quiet config set project woven-fountain-394919
+gcloud --quiet config set container/cluster kuar-cluster
+gcloud --quiet config set compute/zone us-west1-a
+gcloud --quiet container clusters get-credentials kuar-cluster
+
+
+# # gcloud container clusters get-credentials $CLUSTER_NAME --zone $CLUSTER_ZONE --project $PROJECT_ID
+# gcloud container clusters get-credentials kuar-cluster --zone us-west1-a --project woven-fountain-394919
 
 kubectl apply -f kubernetes/deployment.yaml
 
 kubectl apply -f kubernetes/service.yaml
 
-docker rmi zxcv198/flask-example:1.0.4
+docker rmi zxcv198/flask-example:1.0.5
 
 echo "Application deployed successfully!"
