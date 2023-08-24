9ce702452b1031ffba4891339f55c91e8bf692c1
Louis <qwerter198@gmail.com>
2023-08-24 17:56:02 +0800

    fix: 調整deploy git 錯誤

--- deploy-remote-git.sh
+++ deploy-remote-git.sh
@@ -2,9 +2,11 @@
 
 cd /temp/flask-example
 
+git checkout main
+
 git pull
 
-docker-compose -f dokcer-compose.yml down
+docker-compose -f docker-compose.yml down
 
 docker rmi flask-example_web:1.0.0
 
