e7b6dbf2d0d26ff55b20ce9e939c0b1d2b702f40
Louis <qwerter198@gmail.com>
2023-08-24 20:43:44 +0800

    feat: 將image更新為1.0.3測試

--- deploy-remote-k8s.sh
+++ deploy-remote-k8s.sh
@@ -12,10 +12,12 @@ docker-compose -f docker-compose.yml up -d
 
 docker-compose push
 
+sleep 10
+
 kubectl apply -f kubernetes/deployment.yaml
 
 kubectl apply -f kubernetes/service.yaml
 
-docker rmi zxcv198/flask-example:1.0.1
+docker rmi zxcv198/flask-example:1.0.2
 
 echo "Application deployed successfully!"
